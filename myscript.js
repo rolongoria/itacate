eea7df9a3a3bd856d92723d90aed581fd1f26908
rolongoria <ra.longoria86@gmail.com>
2023-09-13 11:19:29 -0500

    Modifier Products consolidated

--- myscript.js
+++ myscript.js
@@ -62,6 +62,26 @@ function printProductSalesFunc(){
         'PUPUSAS RAJAS': allProducts['modifiers']['PUPUSAS Rajas Poblanas'] + allProducts['modifiers']['Pup. Rajas'],
         'PUPUSAS RES/BEEF': allProducts['modifiers']['PUPUSAS Res / Beef'] + allProducts['modifiers']['Pup. Res'],
         'PUPUSAS BEANS & CHEESE': allProducts['modifiers']['PUPUSAS Beans & Cheese'] + allProducts['modifiers']['Pup. Frijol / Queso'],
+        'PUPUSAS CHEESE': allProducts['modifiers']['PUPUSAS Cheese'] + allProducts['modifiers']['Pup. Queso'],
+        'GORDITAS ORIGINAL/PORK': allProducts['modifiers']['GORDITAS Original (Pork)'] + allProducts['modifiers']['Gor. Original (pork)'],
+        'GORDITAS CHICHARRON ROJO': allProducts['modifiers']['GORDITAS Chicharron ROJO'] + allProducts['modifiers']['Gor. Chicharron ROJO'],
+        'GORDITAS CHICHARRON VERDE': allProducts['modifiers']['GORDITAS Chicharron VERDE'] + allProducts['modifiers']['Gor. Chicharron VERDE'],
+        'GORDITAS RES/BEEF': allProducts['modifiers']['GORDITAS Res / Beef'] + allProducts['modifiers']['Gor. Res (beef)'],
+        'GORDITAS BEANS/FRIJOLES': allProducts['modifiers']['GORDITAS Beans / Frijoles'],
+        'GORDITAS BEANS & CHEESE': allProducts['modifiers']['GORDITAS Beans & Cheese'] + allProducts['modifiers']['Gor. Frijol / Queso'],
+        'GORDITAS CHEESE': allProducts['modifiers']['Gor. Queso'] + allProducts['modifiers']['GORDITAS Cheese / Queso'],
+        'LLORARAS ORIGINAL/PORK': allProducts['modifiers']['TACO LLORARAS Lloraras ORIGINAL'] + allProducts['modifiers']['TACO LLORARAS']
+                                + allProducts['modifiers']['Llor. Original'],
+        'LLORARAS VEGGIE': allProducts['modifiers']['TACO LLORARAS Lloraras VEGGIE'] + allProducts['modifiers']['Llor. Veggie'],
+        'TACOS PLANCHA': allProducts['modifiers']['TACO PLANCHA'] + allProducts['modifiers']['Taco Plancha'],
+        'FLAUTAS POLLO': allProducts['modifiers']['FLAUTAS POLLO'],
+        'FLAUTAS RES': allProducts['modifiers']['FLAUTAS RES'] + allProducts['modifiers']['FLAUTAS'],
+        'OVNIS': allProducts['modifiers']['OVNIS'],
+        'CHICHARRON': allProducts['modifiers']['CHICHARRON'],
+        'PANELA': allProducts['modifiers']['PANELA'],
+
+
+
     }
 
     console.log(allSales)
